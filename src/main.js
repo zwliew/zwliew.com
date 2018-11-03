8268eb18e82481b4414caafc825f3d199415534b
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-11-03 01:51:30 +0000

    Use 'q' as an abbreviation for the query function
    To differentiate it from jQuery. q is also easier to type than $.

--- src/main.js
+++ src/main.js
@@ -2,32 +2,32 @@ import query from './query.js';
 import Router from './router.js';
 
 { // Global block scope
-const $ = query(document);
+const q = query(document);
 const router = new Router(window, document, history);
 
 window.addEventListener('load', () => {
   // Initialize the router
-  router.init($);
+  router.init(q);
 
   // Start at whatever valid URL is entered, otherwise at home.
   router.navigate(`${window.location.pathname.replace(/^\/|\/$/g, '')}`, true);
 
   // Home
-  $('#home-social-github').on('click', () => window.open('https://github.com/zwliew'));
-  $('#home-social-email').on('click', () => window.open('mailto:zhaoweiliew@gmail.com'));
-  $('#home-social-medium').on('click', () => window.open('https://medium.com/@zwliew'));
-  $('#home-nav-blog').on('click', () => router.navigate('blog'));
-  $('#home-nav-projects').on('click', () => router.navigate('projects'));
-  $('#home-nav-about').on('click', () => router.navigate('about'));
+  q('#home-social-github').on('click', () => window.open('https://github.com/zwliew'));
+  q('#home-social-email').on('click', () => window.open('mailto:zhaoweiliew@gmail.com'));
+  q('#home-social-medium').on('click', () => window.open('https://medium.com/@zwliew'));
+  q('#home-nav-blog').on('click', () => router.navigate('blog'));
+  q('#home-nav-projects').on('click', () => router.navigate('projects'));
+  q('#home-nav-about').on('click', () => router.navigate('about'));
 
   // Page header
-  $('.page-header-blog').on('click', () => router.navigate('blog', true));
-  $('.page-header-projects').on('click', () => router.navigate('projects', true));
-  $('.page-header-about').on('click', () => router.navigate('about', true));
-  $('.page-header-back').on('click', () => history.back());
+  q('.page-header-blog').on('click', () => router.navigate('blog', true));
+  q('.page-header-projects').on('click', () => router.navigate('projects', true));
+  q('.page-header-about').on('click', () => router.navigate('about', true));
+  q('.page-header-back').on('click', () => history.back());
 
   // Projects
-  $('#projects .list-item').get().forEach(item => (
+  q('#projects .list-item').get().forEach(item => (
     item.addEventListener('click', () => window.open(item.dataset.href))
   ));
 });
