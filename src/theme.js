1848185b30a9d8cd69fc98b19e24a09cb4f5a6af
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-11-07 00:37:25 +0000

    Simplify theme color management

--- src/theme.js
+++ src/theme.js
@@ -5,26 +5,6 @@ const THEMES = deepFreeze({
   night: 'night',
 });
 let curTheme = THEMES.day;
-const THEME_COLORS = deepFreeze({
-  [THEMES.day]: {
-    primaryColor: '#4fc3f7',
-    primaryColorDark: '#0093c4',
-    primaryColorLight: '#8bf6ff',
-    secondaryColor: '#e1f5fe',
-    textColor: '#000000de',
-    primaryColorHover: '#0093c4', // primaryColorDark
-    greyColorHover: '#eee', // greyColorLight
-  },
-  [THEMES.night]: {
-    primaryColor: '#01579b',
-    primaryColorDark: '#002f6c',
-    primaryColorLight: '#58a5f0',
-    secondaryColor: '#039be5',
-    textColor: '#ffffffde',
-    primaryColorHover: '#58a5f0', // primaryColorLight
-    greyColorHover: '#a4a4a4', // greyColorDark
-  },
-});
 
 /**
  * Switches the theme colors for an element
@@ -35,9 +15,7 @@ function switchTheme(theme, element) {
     return;
   }
   curTheme = theme;
-  Object.entries(THEME_COLORS[curTheme])
-    .map(([key, value]) => ['--'.concat(key.split(/(?=[A-Z])/).join('-').toLowerCase()), value])
-    .forEach(([key, value]) => element.style.setProperty(key, value));
+  element.dataset.theme = theme;
 }
 
 const hours = (new Date()).getHours();
