0127f3c397225e17ee0b472f83d4aa51483d6df7
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-11-13 08:58:56 +0000

    Replace navigation header with a route-nav component

---
+++ src/components/route-nav.js
@@ -0,0 +1,99 @@
+import eventBus, { EVENTS } from '/src/eventBus.js';
+import { curTheme, THEMES } from '/src/theme.js';
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    #nav {
+      display: flex;
+      font-family: Nunito, Roboto, Helvetica, Arial, sans-serif;
+      font-size: 1.1rem;
+    }
+
+    .nav-item:nth-of-type(n+2):not(:last-of-type)::before {
+      content: '|';
+      font-weight: normal;
+      margin: 0 4px;
+    }
+
+    .link:hover {
+      cursor: pointer;
+      text-decoration: underline;
+    }
+
+    .link[data-current] {
+      cursor: auto;
+      font-weight: bold;
+      text-decoration: none;
+    }
+
+    .theme {
+      background: var(--primary-color);
+      border: 2px solid var(--grey-color);
+      border-radius: 100%;
+      display: inline-block;
+      height: 24px;
+      margin-left: auto;
+      width: 24px;
+    }
+
+    .theme:hover {
+      cursor: pointer;
+    }
+  </style>
+  <nav id="nav">
+    <span class="link nav-item" data-route="home">home</span>
+    <span class="link nav-item" data-route="notes" >notes</span>
+    <span class="link nav-item" data-route="projects">projects</span>
+    <span class="link nav-item" data-route="about">about</span>
+    <span class="nav-item theme"></span>
+  </nav>
+`;
+
+class RouteNav extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    this._nav = this.shadowRoot.getElementById('nav');
+    this._links = this._nav.getElementsByClassName('link');
+
+    eventBus.register(EVENTS.navigateLate, this._updateCurrent.bind(this));
+  }
+
+  connectedCallback() {
+    this._nav.addEventListener('click', this._onClick);
+  }
+
+  disconnectedCallback() {
+    this._nav.removeEventListener('click', this._onClick);
+  }
+
+  _onClick(event) {
+    if (event.target.hasAttribute('data-current')) {
+      return;
+    }
+    if (event.target.className.includes('link')) {
+      const route = event.target.dataset.route;
+      eventBus.post(EVENTS.navigate, { route });
+    } else {
+      eventBus.post(EVENTS.switchTheme, {
+        theme: curTheme === THEMES.day ? THEMES.night : THEMES.day
+      })
+    }
+  }
+
+  _updateCurrent({ route }) {
+    for (let i = 0; i < this._links.length; i++) {
+      const link = this._links[i];
+      if (link.hasAttribute('data-current') ||
+          link.dataset.route === route) {
+        link.toggleAttribute('data-current');
+      }
+    }
+  }
+}
+
+customElements.define('route-nav', RouteNav);
--- src/main.js
+++ src/main.js
@@ -12,11 +12,3 @@ q('.home-social').click(ev => open(ev.target.dataset.href));
 q('#home-nav-notes').click(() => navigate(ROUTES.notes));
 q('#home-nav-projects').click(() => navigate(ROUTES.projects));
 q('#home-nav-about').click(() => navigate(ROUTES.about));
-
-// Page header
-q('.page-nav-link').click(ev => navigate(ev.target.dataset.href));
-q('.page-nav-theme').click(ev => (
-  eventBus.post(EVENTS.switchTheme, {
-    theme: curTheme === THEMES.day ? THEMES.night : THEMES.day
-  })
-));
